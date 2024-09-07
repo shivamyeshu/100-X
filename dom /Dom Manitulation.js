031bda0980e6a6119320f937fa3a0d5785555934
Shivam <145973348+shivamyeshu@users.noreply.github.com>
2024-09-07 10:19:53 +0000

    add in folder kinda file

---
+++ dom /Dom Manitulation.js
@@ -0,0 +1,8 @@
+// programming interface of web document 
+
+// dom abstract the struct of the document into tree of object allowing you to manipulate the interface of the website 
+// -static // code that doesnot change 
+// -dynamic // code that change curd 
+
+
+
